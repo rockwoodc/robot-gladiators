603b352c3e1bfbfb9314058d24a6b208a9122d6d
rockwoodc <rockwoodce@gmail.com>
2022-06-05 19:30:31 -0400

    added attack pop-ups

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,51 @@
 //this asks the player to name their robot//
 var playerName = window.prompt("what is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
 
-//this creates a function named "fight"//
-function fight() {
-    window.alert("The fight has begun!");
-}
-//fight();
+//this is the players starting health and attack points//
+var playerHealth = 100;
+var playerAttack = 10;
+
+//enemy robot's name, health and attack points//
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//Opening game alert//
+var fight = function() {
+    //alert player that they are starting a new round//
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack
+
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+    //check enemy's health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died! ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining. "
+};
+
+fight();
+
+
